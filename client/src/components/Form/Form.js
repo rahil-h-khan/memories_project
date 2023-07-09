b9dca6aff6c986f510ee02bad150ae05616aa11e
rahil_h_khane <khanrahil786086@gmail.com>
2023-07-10 03:05:18 +0530

    fixed bugs

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 import useStyles from "./styles";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
-import FileBase from 'react-file-base64';
+import FileBase64 from 'react-file-base64';
 import { useDispatch, useSelector } from "react-redux";
 import { createPost, updatePost } from "../../actions/posts";
 
@@ -36,8 +36,8 @@ const Form = ({currentId, setCurrentId}) => {
                 <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={ (e) => setPostData({ ...postData, creator: e.target.value })} />
                 <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={ (e) => setPostData({ ...postData, title: e.target.value })} />
                 <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={ (e) => setPostData({ ...postData, message: e.target.value })} />
-                <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={ (e) => setPostData({ ...postData, tags: e.target.value })} /> 
-                <div className={classes.fileInput}> < FileBase type="file" multiple={false} onDone={({base64}) => setPostData({ ...postData, selectedFile: base64})} /> </div>
+                <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
+                <div className={classes.fileInput}> < FileBase64 type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /> </div>
                 <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth> Submit</Button>
                 <Button variant="contained" color="secondary" size="large" onClick={clear} fullWidth> Clear</Button> 
             </form>
--- server/index.js
+++ server/index.js
@@ -3,21 +3,20 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import postRoutes from './routes/posts.js';
-
+import dotenv from 'dotenv';
 const app = express();
 app.use(cors());
-
+dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 
 app.use('/posts', postRoutes);
-const CONNECTION_URL =
-  "mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.bitpx5s.mongodb.net/?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
     app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
   )
