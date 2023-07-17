2d649e566d098d02452ce50053cf7ab8acf7c806
rahil_h_khane <khanrahil786086@gmail.com>
2023-07-18 01:27:40 +0530

    Timestamp fixed

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -35,12 +35,11 @@ export const createPost = async (req, res) => {
     message,
     selectedFile,
     creator,
-    tags,
+    tags
   });
 
   try {
     await newPostMessage.save();
-
     res.status(201).json(newPostMessage);
   } catch (error) {
     res.status(409).json({ message: error.message });
